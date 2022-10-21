5c879259c19df068e7340438b560e857a367d565
Stefan Ihle <stefanihle@gmx.net>
2022-10-21 13:52:09 +0200

    add banner as well navbar components

---
+++ components/navbar/Navbar.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = props => {
+	const { username } = props;
+
+	return (
+		<div>
+			NavBar
+			<p>{username}</p>
+			<ul>
+				<li>Home</li>
+				<li>My List</li>
+			</ul>
+			<nav>
+				<div>
+					<button>
+						<p>{username}</p>
+					</button>
+					<div>
+						<a>Sign out</a>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
