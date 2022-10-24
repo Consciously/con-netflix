70050467a89991fdfb9d4e7d3b8469ea4843e759
Stefan Ihle <stefanihle@gmx.net>
2022-10-24 13:45:54 +0200

    add default image in case of an error

--- components/card/Card.component.jsx
+++ components/card/Card.component.jsx
@@ -3,7 +3,10 @@ import Image from 'next/image';
 import styles from './Card.module.css';
 
 const Card = props => {
-	const { imgUrl = '/static/clifford.webp', size = 'medium' } = props;
+	const {
+		imgUrl = 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1459&q=80',
+		size = 'medium',
+	} = props;
 
 	const [imgSrc, setImgSrc] = useState(imgUrl);
 
@@ -15,7 +18,9 @@ const Card = props => {
 
 	const handleOnError = () => {
 		console.log('Error');
-		setImgSrc('/static/clifford.webp');
+		setImgSrc(
+			'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1459&q=80',
+		);
 	};
 
 	return (
